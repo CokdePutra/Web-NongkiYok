aa9de22158e37ef33ecff4799a94f22121d383a6
Gung Nanda <gungnands@gmail.com>
2024-08-08 00:55:02 +0800

    fixing kanha code

--- src/components/Table/TableDashboard.jsx
+++ src/components/Table/TableDashboard.jsx
@@ -3,22 +3,22 @@ import React from "react";
 const TableDashboard = () => {
 
   return (
-    <div className="container mx-auto p-4 rounded mb-4 max-w-screen-lg"> {/* Adjust max width */}
+    <div className="container mx-auto p-4 rounded mb-4 max-w-screen-lg">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl kodchasan-bold text-white ">Lokasi Ngopi</h1>
-        <button className="bg-color-yellow hover:bg-button-gray text-color-black hover:text-white font-bold py-2 px-4 rounded">
+        <button className="bg-color-yellow hover:bg-button-gray text-color-black hover:text-white font-bold py-2 px-4 text-m rounded ">
           + Tambahkan Tempat
         </button>
       </div>
-      <table className="min-w-full rounded-md table-fixed bg-white rounded"> {/* Added table-fixed for fixed layout */}
+      <table className="min-w-full rounded-md table-fixed bg-white"> 
         <thead>
           <tr>
-            <th className="w-40 px-4 py-2 text-xs">Nama</th> {/* Reduced width for Nama column */}
-            <th className="w-64 px-4 py-2 text-xs">Deskripsi</th> {/* Reduced width for Deskripsi column */}
-            <th className="w-36 px-4 py-2 text-xs">Harga</th> {/* Reduced width for Rata-rata Harga column */}
-            <th className="w-28 px-4 py-2 text-xs">Latitude</th> {/* Reduced width for Latitude column */}
-            <th className="w-28 px-4 py-2 text-xs">Longitude</th> {/* Reduced width for Longitude column */}
-            <th className="w-24 px-4 py-2 text-xs">Aksi</th> {/* Reduced width for Aksi column */}
+            <th className="w-40 px-4 py-2 text-xs">Nama</th>
+            <th className="w-64 px-4 py-2 text-xs">Deskripsi</th> 
+            <th className="w-36 px-4 py-2 text-xs">Harga</th> 
+            <th className="w-28 px-4 py-2 text-xs">Latitude</th>
+            <th className="w-28 px-4 py-2 text-xs">Longitude</th>
+            <th className="w-24 px-4 py-2 text-xs">Aksi</th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
