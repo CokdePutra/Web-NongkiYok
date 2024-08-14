3cfcabe2947489d61b87f69ba77f052145ef76fa
Gung Nanda <gungnands@gmail.com>
2024-08-14 22:24:18 +0800

    nampilin gambar pada location card

--- src/views/HomeCard.jsx
+++ src/views/HomeCard.jsx
@@ -27,7 +27,6 @@ const HomeCard = () => {
   const handleSortDown = () => {
     setSortOrder('down');
   };
-
   return (
     <>
       <Navbar />
@@ -36,16 +35,16 @@ const HomeCard = () => {
           <Card
             placeId={card.Id_Places}
             key={index}
-            title={card.Name} 
-            imgSrc="./img/Card/image-ex.png" 
-            description={card.Description} 
+            title={card.Name}
+            imgSrc={card.Image ? `./${card.Image}` : './img/Card/image-ex.png'}
+            description={card.Description}
             link={card.Link}
-            price={card.AVG_Price} 
+            price={card.AVG_Price}
             category={card.Category}
           />
         ))}
       </div>
-      <div className="flex gap-1 mt-2 ml-3 mb-1 absolute bottom-[2rem] left-[2rem]">
+      <div className="mt-2 ml-3 mb-4 fixed bottom-0 left-0">
         {sortOrder === 'up' ? (
           <img
             src="./img/Card/Up.png"
