ecf5014db5c63dd1889600d6bd35f692593aab8b
Gung Nanda <gungnands@gmail.com>
2024-09-17 09:28:22 +0800

    email verification template

--- server/app.js
+++ server/app.js
@@ -74,14 +74,17 @@ const transporter = nodemailer.createTransport({
   },
 });
 // ============================== EMAIL TEST =============================
-// console.log("email user", process.env.EMAIL_USER);
-// console.log("Pass", process.env.EMAIL_PASS);
-// console.log("Host", process.env.EMAIL_HOST);
+console.log("email user", process.env.EMAIL_USER);
+console.log("Pass", process.env.EMAIL_PASS);
+console.log("Host", process.env.EMAIL_HOST);
 // let mailOptions = {
-//   from: process.env.EMAIL_USER,
-//   to: "gungnanda14@gmail.com",
+//   from: {
+//     name: "Nongki YOK",
+//     address: process.env.EMAIL_USER,
+//   },
+//   to: ["cokgdeputrawidnyana25@gmail.com", "gungnanda14@gmail.com"],
 //   subject: "Test Email",
-//   text: "This is a test email sent using Nodemailer!",
+//   html: "<h1>Test email</h1><p>This is a test email</p>",
 // };
 
 // transporter.sendMail(mailOptions, (error, info) => {
@@ -175,11 +178,154 @@ app.post("/register", (req, res) => {
         }
 
         // Mengirim email verifikasi
-        const mailOptions = {
-          from: process.env.EMAIL_USER,
+        let mailOptions = {
+          from: {
+            name: "Nongki YOK",
+            address: process.env.EMAIL_USER,
+          },
           to: email,
           subject: "Email Verification",
-          text: `Your verification code is ${verificationToken}. Please verify your email.`,
+          html: `<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>Nongki-YOk</title>
+        
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
+        </head>
+        
+        <body style="
+              margin: 0;
+              font-family: 'Poppins', sans-serif;
+              background: #393E46;
+              font-size: 14px;
+            ">
+            <div style="
+                max-width: 680px;
+                margin: 0 auto;
+                padding: 45px 30px 60px;
+                background: #393E46;
+                background-repeat: no-repeat;
+                background-size: 800px 452px;
+                background-position: top center;
+                font-size: 14px;
+                color: #434343;
+              ">
+        
+                <main>
+                    <div style="
+                    margin: 0;
+                    margin-top: 70px;
+                    padding: 92px 30px 115px;
+                    background: #ffffffee;
+                    border-radius: 30px;
+                    text-align: center;
+                  ">
+                        <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+                            <h1 style="
+                        margin: 0;
+                        font-size: 24px;
+                        font-weight: 500;
+                        color: #1f1f1f;
+                      ">
+                                Your verification code
+                            </h1>
+                            <p style="
+                        margin: 0;
+                        margin-top: 17px;
+                        font-size: 16px;
+                        font-weight: 500;
+                      ">
+                                Hey ,${name}
+                            </p>
+                            <p style="
+                        margin: 0;
+                        margin-top: 17px;
+                        font-weight: 500;
+                        letter-spacing: 0.56px;
+                      ">
+                                Thank you for creating account in Nongki-Yok. Use the following OTP
+                                to complete the procedure to change your email address. OTP is
+                                valid for
+                                <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
+                                Do not share this code with anyone.
+                            </p>
+                            <p style="
+                        margin: 0;
+                        margin-top: 60px;
+                        font-size: 25px;
+                        font-weight: 600;
+                        letter-spacing: 20px;
+                        color: #ba3d4f;
+                      ">
+                        ${verificationToken}
+                            </p>
+                        </div>
+                    </div>
+        
+                    <p style="
+                    max-width: 400px;
+                    margin: 0 auto;
+                    margin-top: 90px;
+                    text-align: center;
+                    font-weight: 500;
+                    color: #ffffff;
+                  ">
+                        Need help? Ask at
+                        <a href="mailto:archisketch@gmail.com"
+                            style="color: #FCBC36; text-decoration: none;">nongki-yok@gungnanda.com</a>
+                        or visit our
+                        <a href="http://localhost:5173/Contact" target="_blank"
+                            style="color: #FCBC36; text-decoration: none;">Help Center</a>
+                    </p>
+                </main>
+        
+                <footer style="
+                  width: 100%;
+                  max-width: 490px;
+                  margin: 20px auto 0;
+                  text-align: center;
+                  border-top: 1px solid #e6ebf1;
+                ">
+                    <p style="
+                    margin: 0;
+                    margin-top: 40px;
+                    font-size: 16px;
+                    font-weight: 600;
+                    color: #FCBC36;
+                  ">
+                        Nongki-Yok
+                    </p>
+                    <p style="margin: 0; margin-top: 8px; color: #ffffff;">
+                        Denpasar No666, Bali, Indonesia
+                    </p>
+                    <div style="margin: 0; margin-top: 16px;">
+                        <a href="" target="_blank" style="display: inline-block;">
+                            <img width="36px" alt="Facebook"
+                                src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook" />
+                        </a>
+                        <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                            <img width="36px" alt="Instagram"
+                                src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram" /></a>
+                        <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                            <img width="36px" alt="Twitter"
+                                src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter" />
+                        </a>
+                        <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                            <img width="36px" alt="Youtube"
+                                src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube" /></a>
+                    </div>
+                    <p style="margin: 0; margin-top: 16px; color: #434343;">
+                        Copyright © 2024 Nongki Yok. All rights reserved.
+                    </p>
+                </footer>
+            </div>
+        </body>
+        
+        </html>`,
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
