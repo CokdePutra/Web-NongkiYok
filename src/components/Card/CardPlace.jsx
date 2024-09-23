08205be6739ff0a6a8ef41ae8de389545cb5b0f4
Gung Nanda <gungnands@gmail.com>
2024-09-23 20:19:26 +0800

    resposive admin dashboard

--- src/components/Card/CardPlace.jsx
+++ src/components/Card/CardPlace.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 
-const CardPlace = ({src,title, desc}) => {
+const CardPlace = ({ src, title, desc }) => {
   return (
-    <div className="w-[319px] bg-white rounded-lg shadow-md p-6 flex flex-col items-center m-10 
-    drop-shadow-2xl border-2 border-color-gold-card justify-center"> 
-      <div className="w-16 h-16 rounded-lg flex items-center justify-center mb-4 ">
-        <img 
-          src={src}
-          alt="Heart Icon"
-          className="w-10 h-[auto] "
-        />
+    <div
+      className="max-w-md w-full bg-white rounded-lg shadow-md p-6 flex flex-col items-center m-4 
+    drop-shadow-2xl border-2 justify-center"
+    >
+      <div className="w-16 h-16 rounded-lg flex items-center justify-center mb-4">
+        <img src={src} alt="Heart Icon" className="w-10 h-auto" />
       </div>
-      <h1 className="text-3xl mb-2 kodchasan-bold">{title}</h1> 
-      <p className="text-gray-600 jura-medium">{desc}</p>
+      <h1 className="text-2xl md:text-3xl mb-2 kodchasan-bold text-center">
+        {title}
+      </h1>
+      <p className="text-gray-600 jura-medium text-center">{desc}</p>
     </div>
-
-    
   );
 };
 
